3e181309e8c2129d6f58550fe92ae4b83ad3c786
TRINIXEI <oladejiifeoluwaeunice@gmail.com>
2023-02-18 03:53:19 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unist.h>
+/**
+ * main - Entry
+ * Return: 1
+ */
+int main(void)0
+{
+	write(1, "and that piece of art is useful - Dora korpar, 2015-10-19\n", 59);
+	return (1);
+}
