bc7ef35997188fb094909233da64f6440eb7ce0f
TRINIXEI <oladejiifeoluwaeunice@gmail.com>
2023-02-24 04:40:06 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print the number from 1 to 100
+ * 3 multiples print Fizz instead of the number
+ * 5 print Buzz instead of number
+ * 3 & 5 print FizzBuzz
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	_
