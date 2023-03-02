69f751d39fe8f561d167d1a43179a3d8cd4ac402
TRINIXEI <oladejiifeoluwaeunice@gmail.com>
2023-03-02 11:42:32 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,15 +12,15 @@ char *rot13(char *s)
 	int j;
 	char data1[] =
 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char data2[] =
-"MOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char datarot[] =
+"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == data1[j])
 			{
-				s[i] = data2[j];
+				s[i] = datarot[j];
 				break;
 			}
 		}
