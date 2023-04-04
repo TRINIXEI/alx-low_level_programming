3a2d2f9a3718ec7cbf1514ac8e5ba04e9e165bd3
TRINIXEI <oladejiifeoluwaeunice@gmail.com>
2023-04-04 11:19:58 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *calm;
+
+if (head == NULL)
+return;
+
+while (*head)
+{
+calm = (*head)->next;
+*head = calm;
+free(*head);
+}
+
+*head = NULL;
+}
